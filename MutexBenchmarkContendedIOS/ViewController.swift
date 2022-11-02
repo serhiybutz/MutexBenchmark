8e4fa2abcd36600da925cd4e8446b8773f961c21
Serhiy Butz <serhiybutz@gmail.com>
2022-11-02 01:11:30 -0400

    Bring everything in line with renaming the account.

--- MutexBenchmarkContendedIOS/ViewController.swift
+++ MutexBenchmarkContendedIOS/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  MutexBenchmarkContendedIOS
 //
-//  Created by Serge Bouts on 4/25/20.
+//  Created by Serhiy Butz on 4/25/20.
 //  Copyright © 2020 iRiZen.com. All rights reserved.
 //
 
--- MutexBenchmarkNonContended/main.swift
+++ MutexBenchmarkNonContended/main.swift
@@ -2,7 +2,7 @@
 //  main.swift
 //  MutexBenchmarkNonContended
 //
-//  Created by Serge Bouts on 4/24/20.
+//  Created by Serhiy Butz on 4/24/20.
 //  Copyright © 2020 irizen.com. All rights reserved.
 //
 
